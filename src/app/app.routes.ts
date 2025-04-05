93531398d1b56bc543f42a7f7d4e44975228548f
Alberto Miguel Garcia Simo <albertomiguelgarciasimo@outlook.com>
2025-04-05 07:48:50 +0200

    Registro Usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { EmpresasListComponent } from './pages/empresas-list/empresas-list.component';
+import { UsuariosFormComponent } from './pages/usuarios-form/usuarios-form.component';
 
 export const routes: Routes = [
 
     {path:"", pathMatch: "full", redirectTo: "home"},
     {path:"home", component: EmpresasListComponent},
+    {path: 'registro', component: UsuariosFormComponent},
     {path:"**", redirectTo: "home"},
 ];
